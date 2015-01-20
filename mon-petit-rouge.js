41a62c0ecdf17a9bf9de7d6bfa17ce6f67d753f4
Ben Trevor <btrevor@8thlight.com>
2015-01-19 20:40:35 -0600

    objects more

--- mon-petit-rouge.js
+++ mon-petit-rouge.js
@@ -1,15 +1,18 @@
 var canvas = null;
 var context = null;
 
-var currentCoords = [100,200];
 var currentDirection = "north";
-var intervalId = 0;
+var gameLoopIntervalId = -1;
+var hounds = [];
+var fox = {x: 100, y: 200};
+var amy = {x: 400, y: 20};
 
 function init() {
     window.addEventListener('keydown', function(e) {
         console.log("key down: " + e.keyCode);
 
         switch(e.keyCode) {
+            // arrow keys to move
         case 38:
             currentDirection = "north";
             break;
@@ -22,49 +25,87 @@ function init() {
         case 39:
             currentDirection = "east";
             break;
-        case 32:
-            window.clearInterval(intervalId);
-            console.log("cleared interval: " + intervalId);
+
+            // q to quit
+        case 81:
+            window.clearInterval(gameLoopIntervalId);
             break;
         }
     }, false);
+
+    for (i = 1; i < 6; i++) {
+        hounds[i - 1] = {x: i * 30, y: i * 40, isMovingTowardsAmy: true};
+    }
 
     startGameLoop(context);
 }
 
 function startGameLoop(context) {
-    intervalId = window.setInterval(drawNextFrame, 100);
+    gameLoopIntervalId = window.setInterval(drawNextFrame, 10);
 }
 
 function drawNextFrame() {
     canvas = document.getElementById('gameCanvas');
     context = canvas.getContext('2d');
 
+    drawBackground(context);
     drawFox(context);
+    drawHounds(context);
+    drawAmy(context);
+}
 
-    console.log('current coords: ' + currentCoords);
+function drawBackground(context) {
+    context.fillStyle = "rgb(200,200,100)";
+    context.fillRect(0, 0, 600, 400);
+}
+
+function drawHounds(context) {
+    context.fillStyle = "rgb(0,0,180)";
+
+    hounds.forEach(function(hound) {
+        updateCoords(hound);
+        context.fillRect(hound.x, hound.y, 25, 25);
+    });
+}
+
+function updateCoords(hound) {
+    if (hound.isMovingTowardsAmy) {
+        console.log('vvvvvvvvv old hound.x: ' + hound.x);
+        hound.x = (hound.x + amy.x) / 3;
+        console.log('^^^^^^^^^ new hound.x: ' + hound.x);
+        hound.y = (hound.y + amy.y) / 3;
+    } else {
+        hound.x = (hound.x + fox.x) / 3;
+        hound.y = (hound.y + fox.y) / 3;
+    }
 }
 
 function drawFox(context) {
     updateCurrentCoordinates();
 
     context.fillStyle = "rgb(200,0,0)";
-    context.fillRect(currentCoords[0], currentCoords[1], 50, 50);
+    context.fillRect(fox.x, fox.y, 20, 20);
+}
+
+function drawAmy(context) {
+    context.fillStyle = "rgb(0,100,100)";
+    context.fillRect(amy.x, amy.y, 20, 20);
 }
 
 function updateCurrentCoordinates() {
+    speed = 1;
     switch(currentDirection) {
     case "north":
-        currentCoords[1] -= 10;
+        fox.y -= speed;
         break;
     case "south":
-        currentCoords[1] += 10;
+        fox.y += speed;
         break;
     case "east":
-        currentCoords[0] += 10;
+        fox.x += speed;
         break;
     case "west":
-        currentCoords[0] -= 10;
+        fox.x -= speed;
         break;
     }
 }
